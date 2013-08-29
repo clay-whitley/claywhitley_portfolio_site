a8a496b1a74e3442c32a122a0fd8c14ecffbc8bd
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-08-29 08:15:29 -0700

    finish ability to switch between project screenshots with javascript

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,11 +1,30 @@
 function initializeProjects(){
-  
+  var firstProjName = $('.project_body').first().data('projectname');
+  $('#project_name').text(firstProjName);
+  $('.project_body').first().show();
+  $('.project_body').first().attr('data-active', 'true');
 }
 
 $(document).ready(function(){
 
-  var firstProjName = $('.project_body').first().data('projectname');
-  $('#project_name').text(firstProjName);
-  $('.project_body').first().show();
+  initializeProjects();
+
+  $('#prev-project').on('click', function(){
+    current = $('.project_body[data-active="true"]');
+    prev = $('.project_body[data-active="true"]').prev('.project_body');
+    if (prev.length > 0) {
+      current.hide().attr('data-active', 'false');
+      prev.show().attr('data-active', 'true');
+    }
+  });
+
+  $('#next-project').on('click', function(){
+    current = $('.project_body[data-active="true"]');
+    next = $('.project_body[data-active="true"]').next('.project_body');
+    if (next.length > 0) {
+      current.hide().attr('data-active', 'false');
+      next.show().attr('data-active', 'true');
+    }
+  });
 
 });
