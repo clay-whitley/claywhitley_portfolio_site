04967f16ed22b8f7cbb0336b46898f3840745ffd
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-08-29 23:08:30 -0700

    update seed file with more screenshots and projects, update html to allow for spaces in the data attribute

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-project1 = Project.create(name: 'Backbone', url: 'localhost', description: 'lorem ipsum')
-project2 = Project.create(name: 'PayPal', url: 'localhost', description: 'lorem ipsum')
-project3 = Project.create(name: 'Rails', url: 'localhost', description: 'lorem ipsum')
+project1 = Project.create(name: 'PayPal: eBay Search', url: 'localhost', description: 'lorem ipsum')
+project2 = Project.create(name: 'PayPal: New Member Tour', url: 'localhost', description: 'lorem ipsum')
+project3 = Project.create(name: 'PayPal: Verify Bank Account', url: 'localhost', description: 'lorem ipsum')
+project4 = Project.create(name: 'Flashcard App: Get Carded', url: 'http://mcflashy.herokuapp.com/', description: 'lorem ipsum')
+project5 = Project.create(name: 'Backbone App: Skill Tracker', url: 'localhost', description: 'lorem ipsum')
 
 project1.screenshots.create(url: '/images/ebay_search.png')
 project2.screenshots.create(url: '/images/nmt_slide1.png')
+project3.screenshots.create(url: '/images/paypal_verify.png')
+project4.screenshots.create(url: '/images/mcflashy.png')
+project5.screenshots.create(url: '/images/lexington.png')
