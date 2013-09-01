48656b8525650b0d76ce7ba7d0dcc9e172669cb6
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-08-31 22:25:16 -0700

    add counter to the slideshow index page

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,6 +1,9 @@
 function initializeProjects(){
   var firstProjName = $('.project_body').first().data('projectname');
-  $('#project_name').text(firstProjName);
+  var firstProjId = $('.project_body').first().data('projectid');
+  var totalProjects = $('.project_body').length;
+  $('#project_name').append(firstProjName);
+  $('#project_id').text(firstProjId + ' of ' + totalProjects);
   $('.project_body').first().show();
   $('.project_body').first().attr('data-active', 'true');
   $('.project_body').first().find('.url-dropdown').slideDown();
@@ -10,8 +13,9 @@ function createEventListeners(){
   $('#prev-project').on('click', function(){
     $current = $('.project_body[data-active="true"]');
     $prev = $current.prev('.project_body');
+    totalProjects = $('.project_body').length;
     if ($prev.length > 0) {
-      $('#project_name').text($prev.data('projectname'));
+      $('#project_name').html($prev.data('projectname')+'<span class="right">'+$prev.data('projectid')+' of '+totalProjects+'</span>');
       $current.hide().attr('data-active', 'false').find('.url-dropdown').hide();
       $prev.show().attr('data-active', 'true').find('.url-dropdown').slideDown();
     }
@@ -20,8 +24,9 @@ function createEventListeners(){
   $('#next-project').on('click', function(){
     $current = $('.project_body[data-active="true"]');
     $next = $current.next('.project_body');
+    totalProjects = $('.project_body').length;
     if ($next.length > 0) {
-      $('#project_name').text($next.data('projectname'));
+      $('#project_name').html($next.data('projectname')+'<span class="right">'+$next.data('projectid')+' of '+totalProjects+'</span>');
       $current.hide().attr('data-active', 'false').find('.url-dropdown').hide();
       $next.show().attr('data-active', 'true').find('.url-dropdown').slideDown();
     }
